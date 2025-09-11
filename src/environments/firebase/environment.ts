265225fbeb8a6a005adf3dd575c63e8e0e2fb546
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2025-09-11 18:31:31 +0200

    Fixed issue with translations

--- src/environments/firebase/environment.ts
+++ src/environments/firebase/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     apiKey: "FIREBASE_API_KEY",
     authDomain: "angular-firebase-accelerator.firebaseapp.com",
     projectId: "angular-firebase-accelerator",
-    storageBucket: "angular-firebase-accelerator.appspot.com",
+    storageBucket: "angular-firebase-accelerator.firebasestorage.app",
     messagingSenderId: "874906824655",
     appId: "1:874906824655:web:c03eee1f08435734ffecba",
     measurementId: "G-2XSMFF38ZS"
