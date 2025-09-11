d28e3bbecc1ab30930e8b173651416ce43957124
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2025-09-11 18:22:57 +0200

    Fixed issue with translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -117,5 +117,5 @@ export class AppModule {
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, AssetsService.BASE_PATH + "i18n/", ".json");
+  return new TranslateHttpLoader(http, AssetsService.BASE_PATH + "i18n/", `.json?v=${environment.buildVersion}`);
 }
