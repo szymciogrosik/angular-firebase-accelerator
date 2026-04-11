a73ab40c4ed4e50d48247a7b0d035e3841d19056
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2026-04-11 18:50:09 +0200

    Added auto resize directive.

---
+++ src/app/_shared-components/auto-resize-directive/auto-resize.directive.ts
@@ -0,0 +1,27 @@
+import {AfterViewInit, ChangeDetectorRef, Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appAutoResize]',
+  standalone: true
+})
+export class AutoResizeDirective implements AfterViewInit {
+
+  @HostListener('input')
+  onInput(): void {
+    this.adjustHeight();
+  }
+
+  constructor(private el: ElementRef<HTMLTextAreaElement>, private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+    this.adjustHeight();
+  }
+
+  private adjustHeight(): void {
+    const textarea = this.el.nativeElement;
+    textarea.style.overflow = 'hidden';
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }
+}
