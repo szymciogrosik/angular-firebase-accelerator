79d23747c908f849b10d5b1ed77206f0a80154d4
szymciogrosik <szymon.jaroslaw.gross@gmail.com>
2026-05-14 10:02:07 +0200

    Added scroll to the top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {CustomTranslateService} from './_services/translate/custom-translate.service';
 import {FooterComponent} from './footer/footer.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {Title} from '@angular/platform-browser';
 import {TranslateModule} from '@ngx-translate/core';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Router, NavigationEnd} from '@angular/router';
 import {APP_CONFIG} from './app.config.token';
 import {MatDatepickerIntl} from '@angular/material/datepicker';
 import {CustomDatepickerIntl} from './_services/util/custom-datepicker-intl.service';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -20,10 +21,12 @@ import {CustomDatepickerIntl} from './_services/util/custom-datepicker-intl.serv
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private readonly translateService = inject(CustomTranslateService);
   private readonly titleService = inject(Title);
-  private readonly appConfig = inject(APP_CONFIG);
+  private readonly appConfig = inject(APP_CONFIG);private readonly router = inject(Router);
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
 
   constructor() {
     const savedLang = localStorage.getItem(this.appConfig.selected_language_key) || this.appConfig.default_language;
@@ -32,6 +35,16 @@ export class AppComponent {
     this.translateService.getPromise('page.title')
       .then(value => this.titleService.setTitle(value))
       .catch(err => console.error(err));
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      if (this.scrollContainer && this.scrollContainer.nativeElement) {
+        this.scrollContainer.nativeElement.scrollTop = 0;
+      }
+    });
   }
 
 }
